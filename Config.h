296df7281c6dff242470f2d92c83bffbc6b6d3a8
lilavagnino <lav22185@uvg.edu.gt>
2024-08-24 15:03:41 -0600

    Create Config.h

---
+++ Config.h
@@ -0,0 +1,9 @@
+#define IO_USERNAME "LuchyLaYoutuber"
+#define IO_KEY "aio_WNfM66h0hMG2kTslXE6f7H2yOPqR"
+
+#define WIFI_SSID "Free wi-fi 2"
+#define WIFI_PASS "luchycool"
+
+#include "AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
